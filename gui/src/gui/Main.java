3d36270db73a7f3adec0306879ebd5bb35a795df
AEI0U <45014664+AEI0U@users.noreply.github.com>
2019-01-08 21:36:27 +0100

    1st commit:Klasse Main fuer die Gui

---
+++ gui/src/gui/Main.java
@@ -0,0 +1,47 @@
+package gui;
+	import javafx.application.Application;
+	import javafx.scene.*;
+	import javafx.scene.control.*;
+	import javafx.scene.layout.*;
+	import javafx.stage.Stage;
+	import javafx.scene.paint.Color;
+	
+	
+public class Main extends Application {
+
+
+	int HOEHE = 800;
+	int BREITE = 1000;
+	Color FARBE = Color.WHITE;
+
+
+@Override
+public void start(Stage primaryStage) throws Exception {
+
+	primaryStage.setTitle("Bilderverwaltung");
+	Scene scene = new Scene (new VBox(),BREITE,HOEHE,FARBE);
+	primaryStage.setResizable(true);
+	//MenueLeiste
+	 MenuBar menuBar = new MenuBar();
+	 //Menu File
+	 Menu menuDatei = new Menu("Datei");
+	 // Menu Hilfe
+     Menu menuHilfe = new Menu("Hilfe");
+     // Menu Ansicht: Dunkel/Hell, Groﬂe/Kleine Kacheln oder Liste ??
+     Menu menuAnsicht = new Menu("Ansicht");
+     menuBar.getMenus().addAll(menuDatei, menuHilfe, menuAnsicht);
+	 ((VBox) scene.getRoot()).getChildren().addAll(menuBar);
+	 
+	
+ 	
+	primaryStage.setScene(scene);
+	primaryStage.show();
+}
+
+public static void main (String[] args)
+{
+	Application.launch(args);
+}
+}
+
+
