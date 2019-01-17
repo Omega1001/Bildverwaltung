f90eafeb61e2baa8c3d495d7cc5bba780da6898d
Jannik Adam <adamjannik1001@gmail.com>
2019-01-17 23:19:46 +0100

    Enhanced Factory and Factory Ini Generator
    Added method for factories that return the interface they are
    implementing.
    Modified ini generator to create a key/value mapping where the
    implementing interface is the key and a comma separated list of Factorie
    names the value

--- Bildverwaltung/bildverwaltung.factory.impl/src/test/java/bildverwaltung/factories/inigenerator/ContainerFactoryIniGenerator.java
+++ Bildverwaltung/bildverwaltung.factory.impl/src/test/java/bildverwaltung/factories/inigenerator/ContainerFactoryIniGenerator.java
@@ -10,8 +10,13 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,40 +34,62 @@ public class ContainerFactoryIniGenerator {
 			throws URISyntaxException, IOException, ClassNotFoundException {
 		List<File> files = getFileList(baseDirs);
 		ClassLoader loader = generateClassLoader(files);
-		List<String> factoryClassNames = new LinkedList<>();
+		Map<String, List<String>> factoryClassNames = new HashMap<>();
 		for (File f : files) {
-			LOG.info("Processing directory {}",f);
-			factoryClassNames.addAll(processDir(f, f.getAbsolutePath().length(), loader));
+			LOG.info("Processing directory {}", f);
+			mergeIntoMap(factoryClassNames, processDir(f, f.getAbsolutePath().length(), loader));
+
 		}
-		PrintWriter out =null;
+		PrintWriter out = null;
 		LOG.info("Writing Factory List");
 		try {
 			if (!output.getParentFile().exists()) {
 				output.getParentFile().mkdirs();
 			}
 			out = new PrintWriter(new FileWriter(output, false));
-			for(String factoryName : factoryClassNames) {
-				out.println(factoryName);
+			for (Entry<String, List<String>> factoryName : factoryClassNames.entrySet()) {
+				out.print(factoryName.getKey());
+				out.print('=');
+				Iterator<String> it = factoryName.getValue().iterator();
+				while (it.hasNext()) {
+					out.print(it.next());
+					if (it.hasNext()) {
+						out.print(',');
+					}
+				}
+				out.println();
 			}
 			out.flush();
 			LOG.info("Finished writing Factory List File");
-		}catch(IOException e) {
-			LOG.error("Error during writing factory list to file {} : ",output,e);
-		}finally {
-			if(out != null) {
+		} catch (IOException e) {
+			LOG.error("Error during writing factory list to file {} : ", output, e);
+		} finally {
+			if (out != null) {
 				out.close();
 			}
 		}
-		
+
 	}
 
-	private static List<String> processDir(File dir, int basePathLength, ClassLoader loader)
+	private static void mergeIntoMap(Map<String, List<String>> target, Map<String, List<String>> source) {
+		for (String key : source.keySet()) {
+			List<String> val = target.get(key);
+			if (val == null) {
+				val = new LinkedList<>();
+			}
+			val.addAll(source.get(key));
+			target.put(key, val);
+		}
+
+	}
+
+	private static Map<String, List<String>> processDir(File dir, int basePathLength, ClassLoader loader)
 			throws ClassNotFoundException {
-		List<String> factoryClassNames = new LinkedList<>();
-		LOG.debug("Processing directory {}",dir);
+		Map<String, List<String>> factoryClassNames = new HashMap<>();
+		LOG.debug("Processing directory {}", dir);
 		for (File f : dir.listFiles()) {
 			if (f.isDirectory()) {
-				factoryClassNames.addAll(processDir(f, basePathLength, loader));
+				factoryClassNames.putAll(processDir(f, basePathLength, loader));
 			} else if (f.getName().endsWith(".class")) {
 				String path = f.getAbsolutePath();
 				path = path.substring(basePathLength + ((path.charAt(0) == '/' || path.charAt(0) == '\\') ? 2 : 1),
@@ -75,11 +102,27 @@ public class ContainerFactoryIniGenerator {
 					if (Factory.class.isAssignableFrom(c)) {
 						LOG.debug("Class is a Factory");
 						try {
-							c.getConstructor();
-							LOG.info("Added Factory {}", c.getName());
-							factoryClassNames.add(c.getName());
-						} catch (NoSuchMethodException e) {
-							LOG.error("Found factory {}, but it has no standard constructor, skipping", c.getName());
+							Factory<?> factory = (Factory<?>) c.newInstance();
+							Class<?> intf = factory.getInterfaceType();
+							if (intf != null && intf.isInterface()) {
+								LOG.info("Added Factory {}", c.getName());
+								List<String> l = factoryClassNames.get(intf.getName());
+								if (l == null) {
+									l = new LinkedList<>();
+								}
+								l.add(c.getName());
+								factoryClassNames.put(intf.getName(), l);
+							} else {
+								LOG.warn(
+										"Found invalid factory {} : interface type is null or not an interface, ignoring",
+										c.getName());
+							}
+						} catch (IllegalAccessException e) {
+							LOG.error("Found factory {}, but it has no public standard constructor, skipping",
+									c.getName(), e);
+						} catch (InstantiationException e) {
+							LOG.error("Found factory {}, but it threw an exception while instanciation, skipping",
+									c.getName(), e);
 						}
 					} else {
 						LOG.debug("Class is not a factory");
@@ -93,7 +136,7 @@ public class ContainerFactoryIniGenerator {
 	}
 
 	private static ClassLoader generateClassLoader(List<File> files) {
-		LOG.debug("Setting up the ClassLoader for directories {}",files);
+		LOG.debug("Setting up the ClassLoader for directories {}", files);
 		URL[] urls = new URL[files.size()];
 		int cut = 0;
 		int i = 0;
