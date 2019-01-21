b8c9fb0d29526e702cce5161a0337e03cdc8dab6
zoralink99 <alessioragusa@web.de>
2019-01-21 19:04:29 +0100

    changed Importgui
    Import window wurde zu klasse, welche eine fertige import Pane zurückgibt

--- Bildverwaltung/bildverwaltung.gui.fx/src/main/java/ImportPane.java
+++ Bildverwaltung/bildverwaltung.gui.fx/src/main/java/ImportPane.java
@@ -1,7 +1,6 @@
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.input.Dragboard;
@@ -9,46 +8,69 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.stage.FileChooser;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
 
-public class ImportWindow extends Stage {
-    private ListView lv = new ListView();
+public abstract class ImportPane {
 
-    ImportWindow(Stage parentStage){
+    /**
+     *  Methode zum aufbauen der importPane
+     *
+     * @param importWindow die stage ,die die ImportPane enthalten soll
+     * @return fertig aufgebaute ImportPane
+     */
+    public static Pane getImportPane(Stage importWindow){
+        //Nodes
+        Pane pn = new Pane();
         BorderPane bp = new BorderPane();
-        Scene sc = new Scene(bp);
+
+        ListView lv = new ListView();
         Button chooseBt = new Button("choose Files");
+        Button confirmBt = new Button("confirm");
+        Button cancelBt = new Button("cancel");
+        HBox buttonBx = new HBox();
+
+        //putting together
+        buttonBx.setSpacing(5.0);
+        buttonBx.setPadding(new Insets(4,4,4,4));
+
+        buttonBx.getChildren().addAll(chooseBt,confirmBt,cancelBt);
+        bp.setCenter(lv);
+        bp.setBottom(buttonBx);
+        pn.getChildren().add(bp);
+
+        //button Eventhandler
         chooseBt.addEventHandler(MouseEvent.MOUSE_CLICKED,(e)->{
             FileChooser fc= new FileChooser();
-            List<File> fileList = fc.showOpenMultipleDialog(this);
+            List<File> fileList = fc.showOpenMultipleDialog(importWindow);
             ObservableList ol = FXCollections.observableArrayList();
             ol.addAll(fileList);
             lv.setItems(ol);
             e.consume();
+
         });
-        Button confirmBt = new Button("confirm");
+
         confirmBt.addEventHandler(MouseEvent.MOUSE_CLICKED,(e)->{
-            ListView lv= getListview();
             List li = Arrays.asList(lv.getItems().toArray());
-
-
+            //PictureImportServiceImpl pi = Container.getActiveContainer().materialize(PictureImportServiceImpl.class,Scope.Application,null);
+            //pi.importAll(li);
+            //todo "PictureImportScrviceImpl" durch api ersetzen und kommentare entfernen
             e.consume();
         });
-        Button cancelBt = new Button("cancel");
+
         cancelBt.addEventHandler(MouseEvent.MOUSE_CLICKED,(e)->{
-            this.close();
+            importWindow.close();
             e.consume();
         });
 
+        //Drag and Drop
         bp.setOnDragOver((e)->{
             if (e.getDragboard().hasFiles()) {
-                /* allow for both copying and moving, whatever user chooses */
                 e.acceptTransferModes(TransferMode.COPY_OR_MOVE);
             }
             e.consume();
@@ -63,25 +85,7 @@ public class ImportWindow extends Stage {
             e.setDropCompleted(true);
             e.consume();
         });
-        HBox buttonBx = new HBox();
-        buttonBx.setSpacing(5.0);
-        buttonBx.setPadding(new Insets(4,4,4,4));
 
-
-        bp.setCenter(lv);
-
-
-        buttonBx.getChildren().addAll(chooseBt,confirmBt,cancelBt);
-        bp.setBottom(buttonBx);
-        this.setScene(sc);
-        this.initOwner(parentStage);
-        this.initModality(Modality.APPLICATION_MODAL);
-        this.setX(parentStage.getX()+20.0);
-        this.setY(parentStage.getY()+20.0);
-        this.showAndWait();
-    }
-
-    private ListView getListview(){
-        return lv;
+        return pn;
     }
 }
