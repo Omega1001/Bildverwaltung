2435889b61f544333aafc0f0a814051b2cf05a12
matz9 <matthias.sennikow@gmail.com>
2019-01-21 11:05:56 +0100

    Added first Test for PictureImportServiceImplTest

--- Bildverwaltung/bildverwaltung.service.impl/src/test/java/PictureImportServiceImplTest.java
+++ Bildverwaltung/bildverwaltung.service.impl/src/test/java/PictureImportServiceImplTest.java
@@ -1,2 +1,31 @@
+import bildverwaltung.service.pictureimport.PictureImportService;
+import bildverwaltung.service.pictureimport.impl.PictureImportServiceImpl;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+import java.io.File;
+import java.net.URISyntaxException;
+
 public class PictureImportServiceImplTest {
+
+    PictureImportService sut = new PictureImportServiceImpl();
+
+    @Test
+    public void testCheckIfPicture() throws URISyntaxException {
+
+        File testFile = new File(PictureImportServiceImpl.class.getResource("/ex_pics/Art-gordon-greybg.jpg").toURI());
+        File testFileNotPicture = new File(PictureImportServiceImpl.class.getResource("/voll_das_picture_und_so").toURI());
+
+        assertTrue(sut.isPicture(testFile));
+        assertFalse(sut.isPicture(testFileNotPicture));
+
+    }
+
+    @Test
+    public void testGetExtension() { }
+
+    @Test
+    public void testImportFile() { }
+
 }
