c686e7919e842638dc64848b2d9fd285eb4221c6
zoralink99 <alessioragusa@web.de>
2019-01-21 08:06:23 +0100

    drag and drop

--- Bildverwaltung/bildverwaltung.gui.fx/src/main/java/ImportWindow.java
+++ Bildverwaltung/bildverwaltung.gui.fx/src/main/java/ImportWindow.java
@@ -6,20 +6,23 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-
 import java.io.File;
+import java.util.Arrays;
 import java.util.List;
 
+
 public class ImportWindow extends Stage {
+    private ListView lv = new ListView();
+
     ImportWindow(Stage parentStage){
         BorderPane bp = new BorderPane();
         Scene sc = new Scene(bp);
-        ListView lv= new ListView();
         Button chooseBt = new Button("choose Files");
         chooseBt.addEventHandler(MouseEvent.MOUSE_CLICKED,(e)->{
             FileChooser fc= new FileChooser();
@@ -30,9 +33,24 @@ public class ImportWindow extends Stage {
             e.consume();
         });
         Button confirmBt = new Button("confirm");
+        confirmBt.addEventHandler(MouseEvent.MOUSE_CLICKED,(e)->{
+            ListView lv= getListview();
+            List li = Arrays.asList(lv.getItems().toArray());
+
+
+            e.consume();
+        });
         Button cancelBt = new Button("cancel");
         cancelBt.addEventHandler(MouseEvent.MOUSE_CLICKED,(e)->{
             this.close();
+            e.consume();
+        });
+
+        bp.setOnDragOver((e)->{
+            if (e.getDragboard().hasFiles()) {
+                /* allow for both copying and moving, whatever user chooses */
+                e.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+            }
             e.consume();
         });
 
@@ -42,6 +60,7 @@ public class ImportWindow extends Stage {
             ObservableList ol = FXCollections.observableArrayList();
             ol.addAll(fileList);
             lv.setItems(ol);
+            e.setDropCompleted(true);
             e.consume();
         });
         HBox buttonBx = new HBox();
@@ -57,6 +76,12 @@ public class ImportWindow extends Stage {
         this.setScene(sc);
         this.initOwner(parentStage);
         this.initModality(Modality.APPLICATION_MODAL);
+        this.setX(parentStage.getX()+20.0);
+        this.setY(parentStage.getY()+20.0);
         this.showAndWait();
+    }
+
+    private ListView getListview(){
+        return lv;
     }
 }
