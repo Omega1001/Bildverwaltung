b1b21416311970f0c0ef6cc9d8c2f4d737fce9d4
matz9 <matthias.sennikow@gmail.com>
2019-01-24 23:05:25 +0100

    implemented the pap about contextcreationalsubscopeid into materialize(), materializeAny and materializeAll need to be implemented like this too, shouldn't be hard at all.

--- Bildverwaltung/bildverwaltung.container.impl/src/main/java/bildverwaltung/container/ManagedContainerImpl.java
+++ Bildverwaltung/bildverwaltung.container.impl/src/main/java/bildverwaltung/container/ManagedContainerImpl.java
@@ -1,70 +1,108 @@
 package bildverwaltung.container;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.*;
 
 public class ManagedContainerImpl implements ManagedContainer {
 
+    //private Map<Scope,ScopeContainer> scopeContainer;
     private Map<Scope,ScopeContainer> scopeContainer;
-    Map<Class,List<Factory<?>>> factories; //save in (interface, factory) pairs
+    private Map<Class,List<Factory<?>>> factories; //save in (interface, factory) pairs
+
+    private UUID creationalContextScopeId;
+
+    private static final Logger LOG = LoggerFactory.getLogger(ManagedContainerImpl.class);
 
     public ManagedContainerImpl() {
         factories = new HashMap<Class, List<Factory<?>>>();
         scopeContainer = new HashMap<>();
+        scopeContainer.put(Scope.APPLICATION, new ScopeContainerImpl(Scope.APPLICATION));
     }
 
     @Override
     public <T> T materialize(Class<T> interfaceClass, Scope scope, UUID scopeId) {
 
-        switch(scope) {
-            case APPLICATION:
+        List desiredFactory = factories.get(interfaceClass);
 
-                if(scopeId != null) {
-                    throw new ContainerException("no subscopes allowed in APPLICATION Scope");
+        if (desiredFactory == null) {
+            LOG.error("given class does not have a factory");
+            throw new ContainerException("given class does not have a factory");
+        } else if (desiredFactory.size() > 1) {
+            // materialize method only usable when there is only one factory max for an given interface
+            LOG.error("ambiguous materialize: more than one factory for this interface");
+            throw new ContainerException("ambiguous materialize: more than one factory for this interface");
+
+            // differentiate the different scopes
+        } else {
+            Factory<?> singleFactory = (Factory<?>) desiredFactory.get(0);
+
+            if (scope == Scope.DEFAULT) {
+                LOG.debug("given scope is " + scope + " so a new object is given");
+
+                return (T) singleFactory.generate(this, scope);
+
+            } else if (scope == Scope.APPLICATION) {
+
+                ScopeContainer container;
+                boolean hadCCSId;
+
+                // scopeId = null gives the primary scope
+                if (scopeId == null) {
+                    container = scopeContainer.get(Scope.APPLICATION);
                 } else {
-
-                    ScopeContainer container = scopeContainer.get(scope);
-                    List desiredFactory = factories.get(interfaceClass);
-
-                    if(desiredFactory == null) {
-                        throw new ContainerException("given class does not have a factory");
-                    }
-
-                    if(desiredFactory.size() > 1) {
-                        throw new ContainerException("ambiguous materialize: more than one factory for this interface");
-                    }
-
-                    if(!container.hasImplementationForFactory( (Factory) desiredFactory.get(0))) {
-                        throw new ContainerException("Scope does not have a implementation for this interface");
-                    } else {
-                        return (T) container.getImplementationForFactory( (Factory) desiredFactory.get(0));
-                    }
+                    container = scopeContainer.get(Scope.APPLICATION).getSubScope(scopeId);
                 }
 
-            case DEFAULT:
+                // Was CreationalContextSubScope already created?
+                if (creationalContextScopeId != null) {
 
-                if(scopeId != null) {
-                    throw new ContainerException("no subscopes allowed in DEFAULT Scope");
+                    LOG.debug("- CCS was already created");
+                    hadCCSId = true;
+
                 } else {
 
-                    List desiredFactory = factories.get(interfaceClass);
+                    LOG.debug("- CCS is not created so it will be created");
+                    hadCCSId = false;
+                    //creationalContextScopeId = scopeContainer.get(scopeId).beginSubScope();
+                    creationalContextScopeId = beginCustomScope(scope);
 
-                    if (desiredFactory == null) {
-                        throw new ContainerException("given class does not have a factory");
-                    }else if(desiredFactory.size() > 1) {
-
-                    	// materialize only usable when there is only one factory max for an given interface
-                        throw new ContainerException("ambiguous materialize: more than one factory for this interface");
-
-                    } else {
-
-                        Factory factory =  (Factory) desiredFactory.get(0);
-                        return (T) factory.generate(this, Scope.DEFAULT);
-
-                    }
                 }
 
-            default:
-            	return null;
+                // First try to get from creationalContext
+                T wantedObject = (T) container
+                        .getImplementationForFactory(singleFactory, creationalContextScopeId);
+
+                // If it does not work, try to get from primary Scope
+                if (wantedObject == null) {
+                    wantedObject = (T) container.getImplementationForFactory(singleFactory);
+                }
+
+                // IF it also not works, generate it
+                if (wantedObject == null) {
+                    wantedObject = (T) singleFactory.generate(this, scope);
+                }
+
+                container.setImplementationForFactory(wantedObject, (Factory<?>) desiredFactory.get(0));
+                container.setImplementationForFactory(wantedObject, (Factory<?>) desiredFactory.get(0), creationalContextScopeId);
+
+                if (!hadCCSId) {
+                    LOG.debug("+ CCS subScope will be destroyed");
+                    container.endSubScope(creationalContextScopeId);
+                    creationalContextScopeId = null;
+                } else {
+                    LOG.debug("+ CCS subScope does not need to be destroyed");
+                }
+
+
+                LOG.info("Object " + wantedObject + " is materialized.");
+                return wantedObject;
+
+            } else {
+                LOG.warn("Given scope is unknown. New added scope or why is it not implemented yet?");
+                return null;
+            }
         }
     }
 
@@ -80,6 +118,11 @@ public class ManagedContainerImpl implements ManagedContainer {
 
     @Override
     public <T> T materializeAny(Class<T> interfaceClass, Scope scope, UUID scopeId) {
+    /*
+
+        NEED REIMPLEMENTATION !!!!
+
+     */
 
         switch(scope) {
             case APPLICATION:
@@ -144,6 +187,11 @@ public class ManagedContainerImpl implements ManagedContainer {
     @Override
     public <T> List<T> materializeAll(Class<T> interfaceClass, Scope scope, UUID scopeId) {
 
+    /*
+
+     NEED REIMPLEMENTATION
+
+     */
 
         switch(scope) {
             case APPLICATION:
@@ -235,17 +283,24 @@ public class ManagedContainerImpl implements ManagedContainer {
     @Override
     public <T> void addFactory(Factory<T> factory, Class<T> targetInterface) {
 
+        if(!targetInterface.isInterface()) {
+            LOG.error("given interface is not actually a interface");
+            throw new ContainerException("given interface is not actually a interface");
+        }
+
         List<Factory<?>> list = factories.get(targetInterface);
 
         if(list == null) {
             list = new ArrayList<Factory<?>>();
-        } else {
-           if(!list.contains(factory)) {
+        }
+        if(!list.contains(factory)) {
                list.add(factory);
-           }
         }
 
+
         factories.put(targetInterface, list);
+
+        System.out.println(factories.get(String.class));
     }
     
     @Override
@@ -258,20 +313,23 @@ public class ManagedContainerImpl implements ManagedContainer {
 
     @Override
     public UUID beginCustomScope(Scope scope) {
-        if(scope == Scope.APPLICATION || scope == Scope.DEFAULT) {
+        if(scope == Scope.DEFAULT) {
             throw new ContainerException("The given scope " + scope.name() + " does not support any subscope");
+        } else if(scope == Scope.APPLICATION){
+            UUID newScopeId = scopeContainer.get(scope).beginSubScope();
+            return  newScopeId;
         } else {
-            //TODO add a functioning implementation for this
-            return null;
+            LOG.error("unknown scope " + scope + " or scope has currently no implementation for subScopes.");
+            throw new ContainerException("unknown scope");
         }
     }
 
     @Override
     public void endCustomScope(Scope scope, UUID scopeId) {
-        if(scope == Scope.APPLICATION || scope == Scope.DEFAULT) {
+        if(scope == Scope.DEFAULT) {
             throw new ContainerException("The given scope " + scope.name() + " does not support any subscope so there is nothing to close");
         } else {
-
+            scopeContainer.get(scope).endSubScope(scopeId);
 		}
     }
 }
--- Bildverwaltung/bildverwaltung.container.impl/src/main/java/bildverwaltung/container/ScopeContainerImpl.java
+++ Bildverwaltung/bildverwaltung.container.impl/src/main/java/bildverwaltung/container/ScopeContainerImpl.java
@@ -4,21 +4,37 @@ import java.util.*;
 
 public class ScopeContainerImpl implements ScopeContainer {
 
-    Map<UUID,ScopeContainerImpl> subScopes;
-    Scope managedScope;
-    Map<Factory<?>, Object> factories; //speichert (factory,implementation) paare
+    private final UUID scopeId;
+    Map<UUID,ScopeContainer> subScopes;
+    private Scope managedScope;
+    private Map<Factory<?>, Object> factories; //speichert (factory,implementation) paare
 
-    public ScopeContainerImpl(Scope scope) {
+    public ScopeContainerImpl(Scope scope, UUID scopeId) {
 
         this.managedScope = scope;
+
         if(scope == Scope.APPLICATION) {
-            subScopes = null;
+
+            subScopes = new HashMap<>();
+            subScopes.put(scopeId, this);
+
         } else {
             subScopes= new HashMap<>();
         }
 
         factories = new HashMap<>();
+        this.scopeId = scopeId;
 
+    }
+
+    public ScopeContainerImpl(Scope scope) {
+        this(scope,null);
+    }
+
+
+
+    public UUID getScopeId() {
+        return scopeId;
     }
 
     @Override
@@ -26,76 +42,58 @@ public class ScopeContainerImpl implements ScopeContainer {
         return managedScope;
     }
 
-    /**
-     * Checks whether the primary scope has a implementation associated with a
-     * specific factory
-     * <p>
-     * This method is does the same thing as
-     * {@link #hasImplementationForFactory(Factory, UUID)} with 'null' as second
-     * parameter
-     *
-     * @param factory that is to be checked
-     * @return if the passed factory produced an implementation that is stored in
-     * the primary scope
-     */
+    public UUID beginSubScope(){
+
+        //TODO do we need it?
+        //Methode bereits in ManagedContainer
+        //Subscopes werden aber anscheinend trotzdem im ScopeContainer gemanaged, also wirds auch hier erstellt
+
+        UUID newSubScopeId = UUID.randomUUID();
+
+        ScopeContainer newSubScope = new ScopeContainerImpl(managedScope,newSubScopeId);
+
+        subScopes.put(newSubScopeId, newSubScope);
+
+        return newSubScopeId;
+
+    }
+
+    @Override
+    public void endSubScope(UUID subScopeId) {
+               Map factories= subScopes.remove(subScopeId).getFactories();
+               factories.forEach((k,v)->{((Factory)k).preDestroy(v);});
+               factories.clear();
+    }
+
+    @Override
+    public ScopeContainer getSubScope(UUID subScopeId) {
+        if(subScopes.containsKey(subScopeId)&& subScopes.get(subScopeId)!= null) {
+            return subScopes.get(subScopeId);
+        } else {
+            throw new ContainerException("Scope does not have a subscope with such ID.");
+        }
+    }
+
     @Override
     public boolean hasImplementationForFactory(Factory<?> factory) {
         return factories.get(factory) != null;
     }
 
-    /**
-     * Checks whether the specified subScope has a implementation associated with a
-     * specific factory <br>
-     *
-     * @param factory    that is to be checked
-     * @param subScopeId id of the subScope to check
-     * @return if the passed factory produced an implementation that is stored in
-     * the primary scope
-     */
     @Override
     public boolean hasImplementationForFactory(Factory<?> factory, UUID subScopeId) {
         return getSubScope(subScopeId).getImplementationForFactory(factory) != null;
     }
 
-    /**
-     * This method returns the implementation associated with the specified factory
-     * from the primary scope<br>
-     * This method does the same as
-     * {@link #getImplementationForFactory(Factory, UUID)} with 'null' as second
-     * parameter
-     *
-     * @param factory the implementation is associated with
-     * @return the associated implementation or null!
-     */
     @Override
     public <T> T getImplementationForFactory(Factory<T> factory) {
-            return (T) factories.get(factory);
+        return (T) factories.get(factory);
     }
 
-    /**
-     * This method returns the implementation associated with the specified factory
-     * from the subScope with the specified subScopeId<br>
-     *
-     * @param factory    the implementation is associated with
-     * @param subScopeId id of the subScope to look into
-     * @return the associated implementation from the specified subScope or null if
-     * no such implementation exists
-     */
     @Override
     public <T> T getImplementationForFactory(Factory<T> factory, UUID subScopeId) {
         return getSubScope(subScopeId).getImplementationForFactory(factory);
     }
 
-    /**
-     * Associates the implementation with the specified factory in the primary
-     * scope<br>
-     * This method does the same as
-     * {@link #setImplementationForFactory(Object, Factory, UUID)} with 'null' as
-     * second parameter
-     *
-     * @param impl    to be associated
-     * @param factory to be associated with
-     */
     @Override
     public void setImplementationForFactory(Object impl, Factory<?> factory) {
         factories.put(factory,impl);
@@ -114,45 +112,8 @@ public class ScopeContainerImpl implements ScopeContainer {
         subScopes.get(subScopeId).getFactories().put(factory,impl);
     }
 
-    /**
-     * This method ends a subScope<br>
-     * If a scope ends, all implementations that are managed in that subScope have
-     * to be dereferenced<br>
-     * Prior to that, the implementation have to be put thru its factories
-     * {@link Factory#preDestroy(Object)} method
-     *
-     * @param subScopeId of the subScope to be ended
-     */
-    @Override
-    public void endSubScope(UUID subScopeId) {
-               Map factories= subScopes.remove(subScopeId).getFactories();
-               factories.forEach((k,v)->{((Factory)k).preDestroy(v);});
-               factories.clear();
-    }
-
     public Map<Factory<?>, Object> getFactories() {
         return factories;
     }
 
-    /*public void beginSubScope(){
-
-        TODO do we need it?
-        Methode bereits in ManagedContainer
-    }*/
-
-    /**
-     * Method to get a SubscopeContainer.
-     *
-     * @param subScopeId UUID which specifies a SubscopeContainer
-     * @throws ContainerException if the ScopeContainer does not contain a SubscopeContainer with specified subScopeId
-     * @return ScopeContainer which belongs to the subScopeId
-     */
-    @Override
-    public ScopeContainer getSubScope(UUID subScopeId) {
-        if(subScopes.containsKey(subScopeId)&& subScopes.get(subScopeId)!= null) {
-            return subScopes.get(subScopeId);
-        }else{
-            throw new ContainerException("Scope does not have a subscope with such ID.");
-        }
-    }
 }
