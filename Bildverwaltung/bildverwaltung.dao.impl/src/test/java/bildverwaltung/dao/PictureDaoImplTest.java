e008112365f000db9550cf058c23e830ffb710c8
Dominik <Dominik@Dominik>
2019-01-30 00:13:17 +0100

    Updated PictureDaoImplTest
    - Entity Manager is unable resolve queries sometimes causing 	errors.

--- Bildverwaltung/bildverwaltung.dao.impl/src/test/java/bildverwaltung/dao/PictureDaoImplTest.java
+++ Bildverwaltung/bildverwaltung.dao.impl/src/test/java/bildverwaltung/dao/PictureDaoImplTest.java
@@ -1,30 +1,30 @@
 package bildverwaltung.dao;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import bildverwaltung.dao.entity.Picture;
+import bildverwaltung.dao.exception.DaoException;
+import bildverwaltung.dao.impl.PictureDaoImpl;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+
 import java.util.Date;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 
 import javax.persistence.EntityManager;
 
-import org.hamcrest.collection.IsEmptyCollection;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import bildverwaltung.dao.entity.Picture;
-import bildverwaltung.dao.exception.DaoException;
-import bildverwaltung.dao.impl.PictureDaoImpl;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
-import static org.hamcrest.MatcherAssert.assertThat;
+import org.hamcrest.collection.IsEmptyCollection;
 import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class PictureDaoImplTest {
 
@@ -91,149 +91,263 @@ public class PictureDaoImplTest {
 	@Test
 	public void saveTest1_1() throws DaoException {
 
+		// save null on non-empty DB
+		
 		fillEM();
+		
 		exception.expect(DaoException.class);
+		
+		em.getTransaction().begin();
 		picDao.save(null);
-//		TODO check DB for unwanted changes
+		em.getTransaction().commit();
+		
+		List<Picture> p = em.createQuery("SELECT p FROM PICTURE p", Picture.class).getResultList();
+		assertThat(p, containsInAnyOrder(pic1, pic2, pic3));
 	}
 
 	@Test
 	public void saveTest1_2() throws DaoException {
 
+		// save new Picture entity on non-empty DB
+		
 		fillEM();
 
+		em.getTransaction().begin();
 		picDao.save(pic4);
-		assertEquals(pic4, em.find(Picture.class, uUID4));
-//		TODO check DB for unwanted changes
+		em.getTransaction().commit();
+		
+		List<Picture> p = em.createQuery("SELECT p FROM PICTURE p", Picture.class).getResultList();
+		assertThat(p, containsInAnyOrder(pic1, pic2, pic3, pic4));
 	}
 
 	@Test
 	public void saveTest1_3() throws DaoException {
 
+		// save multiple new Picture entities on non-empty DB
+		
 		fillEM();
 
+		em.getTransaction().begin();
 		picDao.save(pic4);
 		picDao.save(pic5);
-		assertEquals(pic4, em.find(Picture.class, uUID4));
-		assertEquals(pic5, em.find(Picture.class, uUID5));
-//		TODO check DB for unwanted changes
+		em.getTransaction().commit();
+		
+		List<Picture> p = em.createQuery("SELECT p FROM PICTURE p", Picture.class).getResultList();
+		assertThat(p, containsInAnyOrder(pic1, pic2, pic3, pic4, pic5));
 	}
 
 	@Test
 	public void saveTest1_4() throws DaoException {
+		
+		// save copy of already existent Picture entity on non-empty DB
 
 		fillEM();
 
+		em.getTransaction().begin();
 		picDao.save(pic1);
-		assertEquals(pic1, em.find(Picture.class, uUID1));
-//		TODO check DB for unwanted changes
+		em.getTransaction().commit();
+
+		List<Picture> p = em.createQuery("SELECT p FROM PICTURE p", Picture.class).getResultList();
+		assertThat(p, containsInAnyOrder(pic1, pic2, pic3));
 	}
 
 	@Test
 	public void saveTest1_5() throws DaoException {
 
+		// save multiple copies of a new Picture entity on non-empty DB
+		
 		fillEM();
 
+		em.getTransaction().begin();
 		picDao.save(pic4);
 		picDao.save(pic4);
-		assertEquals(pic4, em.find(Picture.class, uUID4));
-//		TODO check DB for unwanted changes
+		em.getTransaction().commit();
+		
+		List<Picture> p = em.createQuery("SELECT p FROM PICTURE p", Picture.class).getResultList();
+		assertThat(p, containsInAnyOrder(pic1, pic2, pic3, pic4));
 	}
 
 	@Test
 	public void saveTest1_6() throws DaoException {
 
+		// save new version of already existent Picture entity on non-empty DB
+		
 		fillEM();
 
+		em.getTransaction().begin();
 		picDao.save(pic1B);
-		assertEquals(pic1, em.find(Picture.class, uUID1));
-		assertEquals(pic1B, em.find(Picture.class, uUID1));
-//		TODO check DB for unwanted changes
+		em.getTransaction().commit();
+		
+		List<Picture> p = em.createQuery("SELECT p FROM PICTURE p", Picture.class).getResultList();
+		assertThat(p, containsInAnyOrder(pic1B, pic2, pic3));
+	}
+	
+	@Test
+	public void saveTest1_7() throws DaoException {
+
+		// save multiple versions of an already existent Picture entity on non-empty DB
+		
+		fillEM();
+
+		em.getTransaction().begin();
+		picDao.save(pic1);
+		picDao.save(pic1B);
+		em.getTransaction().commit();
+		
+		List<Picture> p = em.createQuery("SELECT p FROM PICTURE p", Picture.class).getResultList();
+		assertThat(p, containsInAnyOrder(pic1B, pic2, pic3));
+	}
+	
+	@Test
+	public void saveTest1_8() throws DaoException {
+
+		// save multiple versions of an already existent Picture entity on non-empty DB
+		
+		fillEM();
+
+		em.getTransaction().begin();
+		picDao.save(pic1B);
+		picDao.save(pic1);
+		em.getTransaction().commit();
+		
+		List<Picture> p = em.createQuery("SELECT p FROM PICTURE p", Picture.class).getResultList();
+		assertThat(p, containsInAnyOrder(pic1, pic2, pic3));
 	}
 
 	@Test
 	public void saveTest2_1() throws DaoException {
+		
+		// save null on empty DB
+		
 		exception.expect(DaoException.class);
+		
+		em.getTransaction().begin();
 		picDao.save(null);
-//		TODO check DB for unwanted changes
+		em.getTransaction().commit();
+		
+		List<Picture> p = em.createQuery("SELECT p FROM PICTURE p", Picture.class).getResultList();
+		assertThat(p, IsEmptyCollection.empty());
 	}
 
 	@Test
 	public void saveTest2_2() throws DaoException {
+		
+		// save new Picture entity on empty DB
 
+		em.getTransaction().begin();
 		picDao.save(pic1);
-		assertEquals(pic1, em.find(Picture.class, uUID1));
-//		TODO check DB for unwanted changes
+		em.getTransaction().commit();
+	
+		List<Picture> p = em.createQuery("SELECT p FROM PICTURE p", Picture.class).getResultList();
+		assertThat(p, containsInAnyOrder(pic1));
 	}
 
 	@Test
 	public void saveTest2_3() throws DaoException {
+		
+		// save multiple new Picture entities on empty DB
 
+		em.getTransaction().begin();
 		picDao.save(pic4);
 		picDao.save(pic5);
-		assertEquals(pic4, em.find(Picture.class, uUID4));
-		assertEquals(pic5, em.find(Picture.class, uUID5));
-//		TODO check DB for unwanted changes
+		em.getTransaction().commit();
+		
+		List<Picture> p = em.createQuery("SELECT p FROM PICTURE p", Picture.class).getResultList();
+		assertThat(p, containsInAnyOrder(pic4, pic5));
+	}
+	
+	@Test
+	public void saveTest2_4() throws DaoException {
+		
+		// save multiple versions of a new Picture entity on empty DB
+
+		em.getTransaction().begin();
+		picDao.save(pic1);
+		picDao.save(pic1B);
+		em.getTransaction().commit();
+		
+		List<Picture> p = em.createQuery("SELECT p FROM PICTURE p", Picture.class).getResultList();
+		assertThat(p, containsInAnyOrder(pic1B));
 	}
 
 	@Test
 	public void getTest1_1() throws DaoException {
 
+		// get null from DB
+		
 		fillEM();
+		
 		exception.expect(DaoException.class);
 		picDao.get(null);
-//		TODO check DB for unwanted changes
+		
+		List<Picture> p = em.createQuery("SELECT p FROM PICTURE p", Picture.class).getResultList();
+		assertThat(p, containsInAnyOrder(pic1, pic2, pic3));
 	}
 
 	@Test
 	public void getTest1_2() throws DaoException {
 
+		// get existent entity (by UUID) from DB
+		
 		fillEM();
 
 		assertEquals(picDao.get(uUID1), em.find(Picture.class, uUID1));
-//		TODO check DB for unwanted changes
+		
+		List<Picture> p = em.createQuery("SELECT p FROM PICTURE p", Picture.class).getResultList();
+		assertThat(p, containsInAnyOrder(pic1, pic2, pic3));
 	}
 
 	@Test
 	public void getTest1_3() throws DaoException {
 
+		// get non-existent entity (by UUID) from DB
+		
 		fillEM();
 
 		assertEquals(picDao.get(uUID5), null);
-//		TODO check DB for unwanted changes
+
+		List<Picture> p = em.createQuery("SELECT p FROM PICTURE p", Picture.class).getResultList();
+		assertThat(p, containsInAnyOrder(pic1, pic2, pic3));
 	}
 
 	@Test
 	public void getTest2_1() throws DaoException {
+		
+		 // get null from empty DB
+		
 		exception.expect(DaoException.class);
+		
 		picDao.get(null);
-//		TODO check DB for unwanted changes
+		
+		List<Picture> p = em.createQuery("SELECT p FROM PICTURE p", Picture.class).getResultList();
+		assertThat(p, IsEmptyCollection.empty());
 	}
 
 	@Test
 	public void getTest2_2() throws DaoException {
+		
+		// get non-existent entity from empty DB
 
-		assertEquals(null, picDao.get(uUID1));
-//		TODO check DB for unwanted changes
+		assertNull(picDao.get(uUID1));
+
+		List<Picture> p = em.createQuery("SELECT p FROM PICTURE p", Picture.class).getResultList();
+		assertThat(p, IsEmptyCollection.empty());
 	}
 
 	@Test
 	public void getAllTest1_1() throws DaoException {
-
+		
+		// get all entities (as list) entities from DB
+		
 		fillEM();
-
-		LinkedList<Picture> list = new LinkedList<Picture>();
-		list.add(pic1);
-		list.add(pic2);
-		list.add(pic3);
 
 		assertThat("List equality without order", picDao.getAll(), containsInAnyOrder(pic1, pic2, pic3));
 	}
 
 	@Test
 	public void getAllTest2_1() throws DaoException {
-
-		LinkedList<Picture> list = new LinkedList<Picture>();
+		
+		// get all entities (as list) from empty DB
 
 		assertThat("List equality without order", picDao.getAll(), IsEmptyCollection.empty());
 	}
@@ -241,47 +355,80 @@ public class PictureDaoImplTest {
 	@Test
 	public void deleteTest1_1() throws DaoException {
 
+		// delete null from DB
+		
 		fillEM();
+		
 		exception.expect(DaoException.class);
+		             
+		em.getTransaction().begin();
 		picDao.delete(null);
+		em.getTransaction().commit();
+		
 		List<Picture> p = em.createQuery("SELECT p FROM PICTURE p", Picture.class).getResultList();
 		assertThat(p, containsInAnyOrder(pic1, pic2, pic3));
 	}
 
 	@Test
 	public void deleteTest1_2() throws DaoException {
+		
+		//delete existent Picture entity from DB
 
 		fillEM();
+		
 		em.getTransaction().begin();
 		picDao.delete(uUID3);
 		em.getTransaction().commit();
-		assertNull(em.find(Picture.class, uUID3));
+		
 		List<Picture> p = em.createQuery("SELECT p FROM Picture p", Picture.class).getResultList();
 		assertThat(p, containsInAnyOrder(pic1, pic2));
 	}
 
 	@Test
 	public void deleteTest1_3() throws DaoException {
+		
+		//delete non-existent Picture entity from DB
 
 		fillEM();
+		
 		exception.expect(DaoException.class);
+		
+		em.getTransaction().begin();
 		picDao.delete(uUID5);
+		em.getTransaction().commit();
+		
 		List<Picture> p = em.createQuery("SELECT p FROM PICTURE p", Picture.class).getResultList();
 		assertThat(p, containsInAnyOrder(pic1, pic2, pic3));
 	}
 
 	@Test
 	public void deleteTest2_1() throws DaoException {
+		
+		// delete null from empty DB
+		
 		exception.expect(DaoException.class);
+		
+		em.getTransaction().begin();
 		picDao.delete(null);
-//		TODO check DB for unwanted changes
+		em.getTransaction().commit();
+
+		List<Picture> p = em.createQuery("SELECT p FROM PICTURE p", Picture.class).getResultList();
+		assertThat(p, IsEmptyCollection.empty());
 	}
 
 	@Test
 	public void deleteTest2_2() throws DaoException {
+		
+		// delete non-existent entity from empty DB
+		
 		exception.expect(DaoException.class);
+		
+		em.getTransaction().begin();
 		picDao.delete(uUID1);
-//		TODO check DB for unwanted changes
+		em.getTransaction().commit();
+
+		List<Picture> p = em.createQuery("SELECT p FROM PICTURE p", Picture.class).getResultList();
+		assertThat(p, IsEmptyCollection.empty());
 	}
 
 }
