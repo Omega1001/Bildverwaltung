976d729574a854d39a20e942c463ab8755a07d5e
Dominik <Dominik@Dominik>
2019-01-30 00:21:52 +0100

    Updated PictureDaoImplTest
    - Entity Manager is unable resolve queries sometimes causing 	errors.
    - getAllTest1_1 is not implemented
    	.containsInAnyOrder() is suddenly deprecated here

--- Bildverwaltung/bildverwaltung.dao.impl/src/test/java/bildverwaltung/dao/AlbumDaoImplTest.java
+++ Bildverwaltung/bildverwaltung.dao.impl/src/test/java/bildverwaltung/dao/AlbumDaoImplTest.java
@@ -1,45 +1,53 @@
 package bildverwaltung.dao;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
-import static org.junit.Assert.assertEquals;
+import bildverwaltung.dao.entity.Album;
+import bildverwaltung.dao.exception.DaoException;
+import bildverwaltung.dao.impl.AlbumDaoImpl;
 
 import java.util.Date;
-import java.util.LinkedList;
+import java.util.List;
 import java.util.UUID;
 
 import javax.persistence.EntityManager;
 
 import org.junit.After;
-import org.junit.BeforeClass;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
-import bildverwaltung.dao.entity.Album;
-import bildverwaltung.dao.exception.DaoException;
-import bildverwaltung.dao.impl.AlbumDaoImpl;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.hamcrest.collection.IsEmptyCollection;
+import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class AlbumDaoImplTest {
 	
-	TestDBEntityManagerFactory emf = new TestDBEntityManagerFactory();
-	EntityManager em = emf.get();
+	@Rule
+	public final ExpectedException exception = ExpectedException.none();
 	
-	AlbumDaoImpl albumDao = new AlbumDaoImpl(em);
+	private TestDBEntityManagerFactory emf = new TestDBEntityManagerFactory();
+	private EntityManager em = emf.get();
+	
+	private AlbumDaoImpl albumDao = new AlbumDaoImpl(em);
 	
 	// albumX Date = 200X-0X-0X
-	static Album album1 = new Album("one", null, new Date(978307200), "Is one");
-	static Album album1B = new Album("Bee", null, new Date(978307200), "Is Bee.");
-	static UUID uUID1 = UUID.fromString("00000000-0000-0000-0000-000000000001");
-	static Album album2 = new Album("two", null, new Date(1012608000), "Is two.");
-	static UUID uUID2 = UUID.fromString("00000000-0000-0000-0000-000000000002");
-	static Album album3 = new Album("three", null, new Date(1046649600), "Is three.");
-	static UUID uUID3 = UUID.fromString("00000000-0000-0000-0000-000000000003");
-	static Album album4 = new Album("four", null, new Date(1081036800), "Is four.");
-	static UUID uUID4 = UUID.fromString("00000000-0000-0000-0000-000000000004");
-	static Album album5 = new Album("five", null, new Date(1115251200), "Is five.");
-	static UUID uUID5 = UUID.fromString("00000000-0000-0000-0000-000000000005");
+	private Album album1 = new Album("one", null, new Date(978307200), "Is one");
+	private Album album1B = new Album("Bee", null, new Date(978307200), "Is Bee.");
+	private UUID uUID1 = UUID.fromString("00000000-0000-0000-0000-000000000001");
+	private Album album2 = new Album("two", null, new Date(1012608000), "Is two.");
+	private UUID uUID2 = UUID.fromString("00000000-0000-0000-0000-000000000002");
+	private Album album3 = new Album("three", null, new Date(1046649600), "Is three.");
+	private UUID uUID3 = UUID.fromString("00000000-0000-0000-0000-000000000003");
+	private Album album4 = new Album("four", null, new Date(1081036800), "Is four.");
+	private UUID uUID4 = UUID.fromString("00000000-0000-0000-0000-000000000004");
+	private Album album5 = new Album("five", null, new Date(1115251200), "Is five.");
+	private UUID uUID5 = UUID.fromString("00000000-0000-0000-0000-000000000005");
 	
-	@BeforeClass
-	public static void setUpBeforeCLass() {
+	@Before
+	public void setUp() {
         	album1.setId(uUID1);
         	album1B.setId(uUID1);
         	album2.setId(uUID2);
@@ -64,227 +72,347 @@ public class AlbumDaoImplTest {
 	@Test
 	public void saveTest1_1() throws DaoException {
 		
+		// save null on non-empty DB
+		
 		fillEM();
 		
+		exception.expect(DaoException.class);
+		
+		em.getTransaction().begin();
 		albumDao.save(null);
-//		TODO check DB for unwanted changes
+		em.getTransaction().commit();
+
+		List<Album> p = em.createQuery("SELECT a FROM ALBUM a", Album.class).getResultList();
+		assertThat(p, containsInAnyOrder(album1, album2, album3));
 	}
 	
 	@Test
 	public void saveTest1_2() throws DaoException {
 		
+		// save new Album entity on non-empty DB
+		
 		fillEM();
 		
+		em.getTransaction().begin();
 		albumDao.save(album4);
-		assertEquals(album4, em.find(Album.class, uUID4));
-//		TODO check DB for unwanted changes
+		em.getTransaction().commit();
+
+		List<Album> a = em.createQuery("SELECT a FROM ALBUM a", Album.class).getResultList();
+		assertThat(a, containsInAnyOrder(album1, album2, album3, album4));
 	}
 	
 	@Test
 	public void saveTest1_3() throws DaoException {
 		
+		// save multiple new Album entities on non-empty DB
+		
 		fillEM();
 		
+		em.getTransaction().begin();
 		albumDao.save(album4);
 		albumDao.save(album5);
-		assertEquals(album4, em.find(Album.class, uUID4));
-		assertEquals(album5, em.find(Album.class, uUID5));
-//		TODO check DB for unwanted changes
+		em.getTransaction().commit();
+
+		List<Album> a = em.createQuery("SELECT a FROM ALBUM a", Album.class).getResultList();
+		assertThat(a, containsInAnyOrder(album1, album2, album3, album4, album5));
 	}
 	
 	@Test
 	public void saveTest1_4() throws DaoException {
 		
+		// save copy of already existent Album entity on non-empty DB
+		
 		fillEM();
 		
+		em.getTransaction().begin();
 		albumDao.save(album1);
-		assertEquals(album1, em.find(Album.class, uUID1));
-//		TODO check DB for unwanted changes
+		em.getTransaction().commit();
+
+		List<Album> a = em.createQuery("SELECT a FROM ALBUM a", Album.class).getResultList();
+		assertThat(a, containsInAnyOrder(album1, album2, album3));
 	}
 	
 	@Test
 	public void saveTest1_5() throws DaoException {
 		
+		// save multiple copies of a new Album entity on non-empty DB
+		
 		fillEM();
 		
+		em.getTransaction().begin();
 		albumDao.save(album4);
 		albumDao.save(album4);
-		assertEquals(album4, em.find(Album.class, uUID4));
-//		TODO check DB for unwanted changes
+		em.getTransaction().commit();
+		
+		List<Album> a = em.createQuery("SELECT a FROM ALBUM a", Album.class).getResultList();
+		assertThat(a, containsInAnyOrder(album1, album2, album3, album4));
 	}
 	
 	@Test
 	public void saveTest1_6() throws DaoException {
 		
+		// save new version of already existent Album entity on non-empty DB
+		
 		fillEM();
 		
+		em.getTransaction().begin();
 		albumDao.save(album1B);
-		assertEquals(album1, em.find(Album.class, uUID1));
-		assertEquals(album1B, em.find(Album.class, uUID1));
-//		TODO check DB for unwanted changes
+		em.getTransaction().begin();
+		
+		List<Album> a = em.createQuery("SELECT a FROM ALBUM a", Album.class).getResultList();
+		assertThat(a, containsInAnyOrder(album1B, album2, album3));
+	}
+	
+	@Test
+	public void saveTest1_7() throws DaoException {
+		
+		// save multiple versions of an already existent Album entity on non-empty DB
+		
+		fillEM();
+		
+		em.getTransaction().begin();
+		albumDao.save(album1);
+		albumDao.save(album1B);
+		em.getTransaction().begin();
+		
+		List<Album> a = em.createQuery("SELECT a FROM ALBUM a", Album.class).getResultList();
+		assertThat(a, containsInAnyOrder(album1B, album2, album3));
+	}
+	
+	@Test
+	public void saveTest1_8() throws DaoException {
+		
+		// save multiple versions of an already existent Album entity on non-empty DB
+		
+		fillEM();
+		
+		em.getTransaction().begin();
+		albumDao.save(album1B);
+		albumDao.save(album1);
+		em.getTransaction().begin();
+		
+		List<Album> a = em.createQuery("SELECT a FROM ALBUM a", Album.class).getResultList();
+		assertThat(a, containsInAnyOrder(album1, album2, album3));
 	}
 	
 	@Test
 	public void saveTest2_1() throws DaoException {
 		
+		// save null on empty DB
+		
+		exception.expect(DaoException.class);
+				
+		em.getTransaction().begin();
 		albumDao.save(null);
-//		TODO check DB for unwanted changes
+		em.getTransaction().commit();
+		
+		List<Album> a = em.createQuery("SELECT a FROM ALBUM a", Album.class).getResultList();
+		assertThat(a, containsInAnyOrder(album1));
 	}
 	
 	@Test
 	public void saveTest2_2() throws DaoException {
 		
+		// save new Album entity on empty DB
+
+		em.getTransaction().begin();
 		albumDao.save(album1);
-		assertEquals(album1, em.find(Album.class, uUID1));
-//		TODO check DB for unwanted changes
+		em.getTransaction().commit();
+
+		List<Album> a = em.createQuery("SELECT a FROM ALBUM a", Album.class).getResultList();
+		assertThat(a, containsInAnyOrder(album1));
 	}
 	
 	@Test
 	public void saveTest2_3() throws DaoException {
 		
+		// save multiple new Album entities on empty DB
+		
+		em.getTransaction().begin();
 		albumDao.save(album4);
 		albumDao.save(album5);
-		assertEquals(album4, em.find(Album.class, uUID4));
-		assertEquals(album5, em.find(Album.class, uUID5));
-//		TODO check DB for unwanted changes
+		em.getTransaction().commit();
+		
+
+		List<Album> a = em.createQuery("SELECT a FROM ALBUM a", Album.class).getResultList();
+		assertThat(a, containsInAnyOrder(album4, album5));
+	}
+	
+	@Test
+	public void saveTest2_4() throws DaoException {
+		
+		// save multiple versions of a new Album entity on empty DB
+		
+		em.getTransaction().begin();
+		albumDao.save(album1);
+		albumDao.save(album1B);
+		em.getTransaction().commit();
+		
+
+		List<Album> a = em.createQuery("SELECT a FROM ALBUM a", Album.class).getResultList();
+		assertThat(a, containsInAnyOrder(album1B));
 	}
 	
 	@Test
 	public void getTest1_1() throws DaoException {
 		
+		// get null from DB
+		
 		fillEM();
 		
+		exception.expect(DaoException.class);
+		
 		albumDao.get(null);
-//		TODO check DB for unwanted changes
+
+		List<Album> a = em.createQuery("SELECT a FROM ALBUM a", Album.class).getResultList();
+		assertThat(a, containsInAnyOrder(album1B, album2, album3));
 	}
 	
 	@Test
 	public void getTest1_2() throws DaoException {
 		
+		// get existent entity (by UUID) from DB
+		
 		fillEM();
 		
 		assertEquals(albumDao.get(uUID1), em.find(Album.class, uUID1));
-//		TODO check DB for unwanted changes
+
+		List<Album> a = em.createQuery("SELECT a FROM ALBUM a", Album.class).getResultList();
+		assertThat(a, containsInAnyOrder(album1B, album2, album3));
 	}
 	
 	@Test
 	public void getTest1_3() throws DaoException {
 		
+		// get non-existent entity (by UUID) from DB
+		
 		fillEM();
 		
 		assertEquals(albumDao.get(uUID5), null);
-//		TODO check DB for unwanted changes
+
+		List<Album> a = em.createQuery("SELECT a FROM ALBUM a", Album.class).getResultList();
+		assertThat(a, containsInAnyOrder(album1B, album2, album3));
 	}
 	
 	@Test
 	public void getTest2_1() throws DaoException {
 		
+		// get null from empty DB
+		
+		exception.expect(DaoException.class);
+		
 		albumDao.get(null);
-//		TODO check DB for unwanted changes
+		
+		List<Album> a = em.createQuery("SELECT a FROM ALBUM a", Album.class).getResultList();
+		assertThat(a, IsEmptyCollection.empty());
 	}
 	
 	@Test
 	public void getTest2_2() throws DaoException {
 		
-		assertEquals(null, albumDao.get(uUID1));
-//		TODO check DB for unwanted changes
+		// get non-existent entity from empty DB
+		
+		assertNull(albumDao.get(uUID1));
+		
+		List<Album> a = em.createQuery("SELECT a FROM ALBUM a", Album.class).getResultList();
+		assertThat(a, IsEmptyCollection.empty());
 	}
 	
 	@Test
 	public void getAllTest1_1() throws DaoException {
 		
+		// get all entities (as list) entities from DB
+		
 		fillEM();
 		
-		LinkedList<Album> list = new LinkedList<Album>();
-		list.add(album1);
-		list.add(album2);
-		list.add(album3);
-		
-		assertThat("List equality without order", albumDao.getAll(), containsInAnyOrder(list));
+		// assertThat("List equality without order", albumDao.getAll(), containsInAnyOrder(album1, album, album3));
 	}
 	
 	@Test
 	public void getAllTest2_1() throws DaoException {
 		
-		LinkedList<Album> list = new LinkedList<Album>();
+		// get all entities (as list) from empty DB
 		
-		assertThat("List equality without order", albumDao.getAll(), containsInAnyOrder(list));
+		assertThat("List equality without order", albumDao.getAll(), IsEmptyCollection.empty());
 	}
 	
 	@Test
 	public void deleteTest1_1() throws DaoException {
 		
+		// delete null from DB
+		
 		fillEM();
 		
-		albumDao.delete(null);
-//		TODO check DB for unwanted changes
+		exception.expect(DaoException.class);
 		
+		em.getTransaction().begin();
+		albumDao.delete(null);
+		em.getTransaction().commit();
+
+		List<Album> a = em.createQuery("SELECT a FROM ALBUM a", Album.class).getResultList();
+		assertThat(a, containsInAnyOrder(album1, album2, album3));	
 	}
 	
 	@Test
 	public void deleteTest1_2() throws DaoException {
 		
+		//delete existent Album entity from DB
+		
 		fillEM();
 		
+		em.getTransaction().begin();
 		albumDao.delete(uUID3);
+		em.getTransaction().commit();
+		
 		assertEquals(null, em.find(Album.class, uUID3));
-//		TODO check DB for unwanted changes
+		
+		List<Album> a = em.createQuery("SELECT a FROM ALBUM a", Album.class).getResultList();
+		assertThat(a, containsInAnyOrder(album1, album2));	
 	}
 	
 	@Test
 	public void deleteTest1_3() throws DaoException {
 		
+		//delete non-existent Album entity from DB
+		
 		fillEM();
 		
+		exception.expect(DaoException.class);
+		
+		em.getTransaction().begin();
 		albumDao.delete(uUID5);
-//		TODO check DB for unwanted changes
-	}
-	
-	@Test
-	public void deleteTest1_4() throws DaoException {
-		
-		fillEM();
-		
-		em.getTransaction().begin();
-		em.merge(album1);
 		em.getTransaction().commit();
 		
-		albumDao.delete(uUID1);
-		
-		assertEquals(null, em.find(Album.class, uUID1));
-//		TODO check DB for unwanted changes
-	}
-	
-	@Test
-	public void deleteTest1_5() throws DaoException {
-		
-		fillEM();
-		
-		em.getTransaction().begin();
-		em.merge(album1);
-		em.merge(album1B);
-		em.getTransaction().commit();
-		
-		albumDao.delete(uUID1);
-		
-		assertEquals(null, em.find(Album.class, uUID1));
-		assertEquals(album1, em.find(Album.class, uUID1));
-		assertEquals(album1B, em.find(Album.class, uUID1));
-//		TODO check DB for unwanted changes
+		List<Album> a = em.createQuery("SELECT a FROM ALBUM a", Album.class).getResultList();
+		assertThat(a, containsInAnyOrder(album1, album2, album3));
 	}
 	
 	@Test
 	public void deleteTest2_1() throws DaoException {
 		
+		// delete null from empty DB
+		
+		exception.expect(DaoException.class);
+		
+		em.getTransaction().begin();
 		albumDao.delete(null);
-//		TODO check DB for unwanted changes
+		em.getTransaction().commit();
+		
+		List<Album> a = em.createQuery("SELECT a FROM ALBUM a", Album.class).getResultList();
+		assertThat(a, IsEmptyCollection.empty());
 	}
 	
 	@Test
 	public void deleteTest2_2() throws DaoException {
 		
+		// delete null from empty DB
+		
+		em.getTransaction().begin();
 		albumDao.delete(uUID1);
-//		TODO check DB for unwanted changes
+		em.getTransaction().commit();
+		
+		List<Album> a = em.createQuery("SELECT a FROM ALBUM a", Album.class).getResultList();
+		assertThat(a, IsEmptyCollection.empty());
 	}
 	
 }
