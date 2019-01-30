cd9dfeec24a59b58829c528ad7d2739e6a5761cb
AEI0U <45014664+AEI0U@users.noreply.github.com>
2019-01-30 20:16:17 +0100

    Update KontextMenue.java

--- Bildverwaltung/bildverwaltung.gui.fx/src/main/java/bildverwaltung/gui/fx/KontextMenue.java
+++ Bildverwaltung/bildverwaltung.gui.fx/src/main/java/bildverwaltung/gui/fx/KontextMenue.java
@@ -25,30 +25,44 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 	 
 	public class KontextMenue extends Application { 
+		private static final int SPACING = 5;
+		private static final String SAVE = "Speichern";
+		private static final String SAVE_AS = "Speichern Unter";
+		private static final String MOVE_PIC = "In Album Verschieben";
+		private static final String CHANGE = "Ã„ndern";
+		private static final String CHANGE_NAME = "Namen Ã„ndern";
+		private static final String CHANGE_COMMENT = "Kommentar Ã„ndern";
+		private static final String CHANGE_INFOS = "Informationen Ã¤ndern";
+		private static final String DELETE = "LÃ¶schen";
+		private static final String DELETE_SINGLE = "Einzeln LÃ¶schen";
+		private static final String DELETE_EVERYWHERE = "Dieses Bild komplett LÃ¶schen";
+		private static final String DELETE_ALL = "Alle Bilder LÃ¶schen";	
+		private static final String WARNING_SINGLE = "Wollen Sie dieses Bild hier lÃ¶schen?";
+		private static final String WARNING_EVERYWHERE = "Wollen Sie dieses Bild hier komplett entfernen";
+		private static final String WARNING_ALL = "Wollen Sie alle Bilder hier lÃ¶schen?";
+		private static final String WARNING_TITLE = "SICHERHEITSFRAGE: LÃ–SCHEN";	 
 	    @Override
 	    public void start(Stage stage) {
 	 
 	        Label label = new Label();
 	 
-	        	//Test mit Kreis:
+	        //Test mit Kreis, die Bilder muessen erst noch auswaehlbar werden.
 	        Circle image = new Circle();
 	        image.setRadius(80);
 	        image.setFill(Color.GREY);
 	 
 	        VBox root = new VBox();
-	        root.setPadding(new Insets(5));
-	        root.setSpacing(5);
+	        root.setPadding(new Insets(SPACING));
+	        root.setSpacing(SPACING);
 	 
 	        root.getChildren().addAll(label, image);
 	        
 	        ContextMenu contextMenu = new ContextMenu();
 	 
-	        Menu menuSave = new Menu("Speichern");
-	        MenuItem saveAs = new MenuItem("Speichern Unter");
-	        MenuItem movePic= new MenuItem("In Album Verschieben");
+	        Menu menuSave = new Menu(SAVE);
+	        MenuItem saveAs = new MenuItem(SAVE_AS);
+	        MenuItem movePic= new MenuItem(MOVE_PIC);
 	        menuSave.getItems().addAll(saveAs, movePic);
-	        //menuSave.getItems().add(new CheckMenuItem("Speichern Unter"));
-	        //menuSave.getItems().add(new CheckMenuItem("In Album Verschieben"));
 	        saveAs.setOnAction(new EventHandler<ActionEvent>() {
 	        
 	        	
@@ -67,10 +81,10 @@ import javafx.stage.Stage;
 	            }
 	        });
 	        
-	        Menu menuAttribut = new Menu("Ändern");
-	        MenuItem changeName = new MenuItem("Namen Ändern");
-	        MenuItem changeComment = new MenuItem("Kommentar ändern");
-	        MenuItem changeInfos = new MenuItem("Informationen ändern"); 
+	        Menu menuAttribut = new Menu(CHANGE);
+	        MenuItem changeName = new MenuItem(CHANGE_NAME);
+	        MenuItem changeComment = new MenuItem(CHANGE_COMMENT);
+	        MenuItem changeInfos = new MenuItem(CHANGE_INFOS); 
 	        menuAttribut.getItems().addAll(changeName, changeComment, changeInfos);
 	        
 	        changeName.setOnAction(new EventHandler<ActionEvent>() {
@@ -101,31 +115,67 @@ import javafx.stage.Stage;
 	        });
 	        
 	        
-	        Menu menuDelete = new Menu("Löschen");
-	        MenuItem deleteSingle = new MenuItem("Einzeln Löschen"); 
-	        MenuItem deleteAll = new MenuItem("Überall Löschen"); 
-	        menuDelete.getItems().addAll(deleteSingle, deleteAll);
+	        Menu menuDelete = new Menu(DELETE);
+	        MenuItem deleteSingle = new MenuItem(DELETE_SINGLE); 
+	        MenuItem deleteAll = new MenuItem(DELETE_ALL); 
+		MenuItem deleteEverywhere= new MenuItem(DELETE_EVERYWHERE); 
+	        menuDelete.getItems().addAll(deleteSingle, deleteEverywhere, deleteAll);
 	        
 	        deleteSingle.setOnAction(new EventHandler<ActionEvent>() {
 	        
 	            @Override
 	            public void handle(ActionEvent event) {
 	               //Einzelnes Bild loeschen
+			Alert alert = new Alert(AlertType.CONFIRMATION);
+		        alert.setTitle(WARNING_TITLE);
+		        alert.setHeaderText(WARNING_SINGLE);
+
+		         Optional<ButtonType> result = alert.showAndWait();
+		          if (result.get() == ButtonType.OK){
+		          	//Bild loeschen
+		          } else {
+		          	// bei Abbrechen und Meldung schliessen: passiert nichts
+		          }
 	            }
 	        });
-	 
-	        deleteAll.setOnAction(new EventHandler<ActionEvent>() {
+	 	deleteEverywhere.setOnAction(new EventHandler<ActionEvent>() {
 		        
 	            @Override
 	            public void handle(ActionEvent event) {
 	               //Bild komplett aus der Datenbank loeschen
-	            	//Sicherhaitsfrage? PopUp: Wirklich Löschen?
+	            	Alert alert = new Alert(AlertType.CONFIRMATION);
+		            	alert.setTitle(WARNING_TITLE);
+		            	alert.setHeaderText(WARNING_EVERYWHERE);
+
+		            	Optional<ButtonType> result = alert.showAndWait();
+		            	if (result.get() == ButtonType.OK){
+		            		//Bild loeschen
+		            	} else {
+		            		// bei Abbrechen und Meldung schliessen: passiert nichts
+		            	}
+	            }
+	        });
+	        deleteAll.setOnAction(new EventHandler<ActionEvent>() {
+		        
+	            @Override
+	            public void handle(ActionEvent event) {
+	               //Alle Bilder loeschen; aber nur aus diesem Album, bzw, mit diesem Filterkriterien.. 
+	            	Alert alert = new Alert(AlertType.CONFIRMATION);
+		            	alert.setTitle(WARNING_TITLE);
+		            	alert.setHeaderText(WARNING_ALL);
+
+		            	Optional<ButtonType> result = alert.showAndWait();
+		            	if (result.get() == ButtonType.OK){
+		            		//Bild loeschen
+		            	} else {
+		            		// bei Abbrechen und Meldung schliessen: passiert nichts
+		            	}
 	            }
 	        });
 	 
 	        contextMenu.getItems().addAll(menuSave, menuAttribut, menuDelete);
 	
-	        // Rechtsklick Event Menue
+	        
 	        image.setOnContextMenuRequested(new EventHandler<ContextMenuEvent>() {
 	 
 	            @Override
