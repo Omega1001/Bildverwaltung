f2ac1ce13bb3a38ba6c687f4d869c2f2f0571174
Matthias Sennikow <matthias.sennikow@gmail.com>
2019-02-03 12:53:39 +0100

    changed ImportService to ImportFacade

--- Bildverwaltung/bildverwaltung.gui.fx/src/main/java/bildverwaltung/gui/fx/Import/ImportPane.java
+++ Bildverwaltung/bildverwaltung.gui.fx/src/main/java/bildverwaltung/gui/fx/Import/ImportPane.java
@@ -21,7 +21,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
-import bildverwaltung.service.*;
+
 
 public class ImportPane{
     private final Messenger msg;
