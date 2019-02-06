9a11ecccb634f6a061a7122d0d8b4a77dd99598d
AEI0U <45014664+AEI0U@users.noreply.github.com>
2019-02-06 23:42:52 +0100

    Update ShowPicture.java

--- Bildverwaltung/bildverwaltung.gui.fx/src/main/java/bildverwaltung/gui/fx/masterview/ShowPicture.java
+++ Bildverwaltung/bildverwaltung.gui.fx/src/main/java/bildverwaltung/gui/fx/masterview/ShowPicture.java
@@ -21,6 +21,7 @@ import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class ShowPicture extends Application{
+	
 		private static final String SAVE = "Speichern";
 		private static final String LAST_PIC = "Letztes Bild";
 		private static final String FULL_PIC = "Vollbild";
@@ -39,91 +40,87 @@ public class ShowPicture extends Application{
 		@Override
 		public void start(final Stage stage) throws Exception {
 			
-			try {
-					stage.setResizable(false);
- 			        
-					ToolBar toolBarTop = new ToolBar();
-					Button buttonSave = new Button(SAVE);
-					
-					toolBarTop.getItems().addAll(buttonSave);
-				
-					showButtonwithFunction(buttonSave, LAST_PIC, MENU_BUTTON_SIZE);
-				
-				   //Bottom toolBar contains buttons to select a pic, add a comment or delete it 
-				    ToolBar toolBarBottom = new ToolBar();
-				    
-					//TODO "<" ist nur ein Platzhalter
-				
-				   //show last Picture 
-				    Button buttonBack = new Button("<");
-				    Image imageDecline = new Image(getClass().getResourceAsStream("Zurueck.png"));
-				    buttonBack.setGraphic(new ImageView(imageDecline));     
-			       // buttonBack.setGraphic(new ImageView("Zurück.png"));
-				    showButtonwithFunction(buttonBack, LAST_PIC, BUTTON_SIZE);
-				    buttonBack.setOnAction(new EventHandler<ActionEvent>() {	  
+			stage.setResizable(false);
+	        
+			ToolBar toolBarTop = new ToolBar();
+			Button buttonSave = new Button(SAVE);
 			
-			            public void handle(ActionEvent event) {
-			                //letztes Bild anzeigen
-			            }
-			        });
-				    
-				    			    
-				    // show the next Picture
-				    //TODO ">" ist nur ein Platzhalter
-				    Button buttonNext = new Button (">");
-				    showButtonwithFunction(buttonNext, NEXT_PIC, BUTTON_SIZE);
-				    buttonNext.setOnAction(new EventHandler<ActionEvent>() {
-			        	public void handle(ActionEvent event) {
-			        		
-			        }});
-				    
-				    HBox centerButtons = new HBox();
-				    centerButtons.setSpacing(BTN_SELECT_SPACING);
-				    centerButtons.setMaxWidth(3.0*BUTTON_SIZE);
-				   
-				    centerButtons.getChildren().addAll(buttonBack,buttonNext);
-				   // Comment
-				    //TODO "C" ist nur ein Platzhalter
-				    Button buttonComment = new Button ("C");
-				    showButtonwithFunction(buttonComment, COMMENTAR, BUTTON_SIZE);
-				    buttonComment.setOnAction(new EventHandler<ActionEvent>() {
-			        	public void handle(ActionEvent event) {
-			        		
-			        }});
-				    
-				    //Delete the shown Image 	
-				    //TODO "X" ist nur ein Platzhalter
-				    Button buttonDelete = new Button ("X");
-				    showButtonwithFunction(buttonDelete, DELETE_PIC, BUTTON_SIZE);
-				    buttonDelete.setOnAction(new EventHandler<ActionEvent>() {
-			        	public void handle(ActionEvent event) {		            	
-						
-			        	
-			            }
-			        });
-				    
-				    HBox components = new HBox();
-				    components.getChildren().addAll(centerButtons, buttonComment,buttonDelete);
-				    components.setSpacing(90.0);
-				    
-				    HBox position = new HBox();
-				    position.setAlignment(Pos.BASELINE_LEFT);
-				    position.setMinWidth(220.0);
-				   
-				    toolBarBottom.getItems().addAll(position,components);
-				    
-				    
-					BorderPane borderPane = new BorderPane();
-				 	borderPane.setTop(toolBarTop);
-				 	borderPane.setBottom(toolBarBottom);
-				 	Scene scene = new Scene(borderPane, WIDTH, HEIGHT);
-				 	
-					stage.setScene(scene);
-				    stage.show();
-			}
-			catch(Exception e){
-				
-			    }
+			toolBarTop.getItems().addAll(buttonSave);
+		
+			showButtonwithFunction(buttonSave, LAST_PIC, MENU_BUTTON_SIZE);
+		
+		   //Bottom toolBar contains buttons to select a pic, add a comment or delete it 
+		    ToolBar toolBarBottom = new ToolBar();
+		    
+		
+		   //show last Picture 
+		    Button buttonBack = new Button("<");
+		    //TODO Image imageBack = new Image(getClass().getResourceAsStream("Bildverwaltung/bildverwaltung.gui.fx/src/main/resources/Zurueck.png"));
+		    //buttonBack.setGraphic(new ImageView(imageBack));   
+		    showButtonwithFunction(buttonBack, LAST_PIC, BUTTON_SIZE);
+		    buttonBack.setOnAction(new EventHandler<ActionEvent>() {	  
+	
+	            public void handle(ActionEvent event) {
+	                //letztes Bild anzeigen
+	            }
+	        });
+		    
+		    			    
+		    // show the next Picture
+		   
+		    Button buttonNext = new Button (">");
+		  //TODO Image imageNext = new Image(getClass().getResourceAsStream("Bildverwaltung/bildverwaltung.gui.fx/src/main/resources/Weiter.png"));
+		    //buttonBack.setGraphic(new ImageView(imageNext)); 
+		    showButtonwithFunction(buttonNext, NEXT_PIC, BUTTON_SIZE);
+		    buttonNext.setOnAction(new EventHandler<ActionEvent>() {
+	        	public void handle(ActionEvent event) {
+	        }});
+		    
+		    HBox centerButtons = new HBox();
+		    centerButtons.setSpacing(BTN_SELECT_SPACING);
+		    centerButtons.setMaxWidth(3.0*BUTTON_SIZE);		   
+		    centerButtons.getChildren().addAll(buttonBack,buttonNext);
+		    
+		   // Comment
+		    Button buttonComment = new Button ("C");
+		  //TODO Image imageComment = new Image(getClass().getResourceAsStream("Bildverwaltung/bildverwaltung.gui.fx/src/main/resources/Kommentar.png"));
+		    //buttonBack.setGraphic(new ImageView(imageComment)); 
+		    showButtonwithFunction(buttonComment, COMMENTAR, BUTTON_SIZE);
+		    buttonComment.setOnAction(new EventHandler<ActionEvent>() {
+	        	public void handle(ActionEvent event) {
+	        		//TODO Comment
+	        }});
+		    
+		    //Delete the shown Image 	
+		    Button buttonDelete = new Button ("X");
+		    //TODO Image imageDelete = new Image(getClass().getResourceAsStream("Bildverwaltung/bildverwaltung.gui.fx/src/main/resources/Loeschen.png"));
+		    //buttonBack.setGraphic(new ImageView(imageDelete)); 
+		    showButtonwithFunction(buttonDelete, DELETE_PIC, BUTTON_SIZE);
+		    buttonDelete.setOnAction(new EventHandler<ActionEvent>() {
+	        	public void handle(ActionEvent event) {		            	
+	        		//TODO Bild loeschen
+	            }
+	        });
+		    
+		    HBox components = new HBox();
+		    components.getChildren().addAll(centerButtons, buttonComment,buttonDelete);
+		    components.setSpacing(90.0);
+		    
+		    HBox position = new HBox();
+		    position.setAlignment(Pos.BASELINE_LEFT);
+		    position.setMinWidth(220.0);
+		   
+		    toolBarBottom.getItems().addAll(position,components);
+		    
+		    
+			BorderPane borderPane = new BorderPane();
+		 	borderPane.setTop(toolBarTop);
+		 	borderPane.setBottom(toolBarBottom);
+		 	Scene scene = new Scene(borderPane, WIDTH, HEIGHT);
+		 	
+			stage.setScene(scene);
+		    stage.show();
+
 		}
 
 		private static void showButtonwithFunction(Button btn, String viewText, double size) {
