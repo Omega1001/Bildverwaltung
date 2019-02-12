a2cb723a15772bc3ae756ee65b4f00625b6d1260
matz9 <matthias.sennikow@gmail.com>
2019-02-12 19:50:59 +0100

    added main part of logic + begun refactoring
    Signed-off-by: matz9 <matthias.sennikow@gmail.com>

--- Bildverwaltung/bildverwaltung.gui.fx/src/main/java/bildverwaltung/gui/fx/attributeEditor/AttributeEditor.java
+++ Bildverwaltung/bildverwaltung.gui.fx/src/main/java/bildverwaltung/gui/fx/attributeEditor/AttributeEditor.java
@@ -1,58 +1,159 @@
 package bildverwaltung.gui.fx.attributeEditor;
 
+import bildverwaltung.container.Container;
+import bildverwaltung.dao.entity.Picture;
+import bildverwaltung.dao.exception.FacadeException;
+import bildverwaltung.facade.PictureFacade;
+import bildverwaltung.localisation.Messenger;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.TilePane;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.time.ZoneId;
+import java.util.Date;
+
 public class AttributeEditor {
     private BorderPane bp;
-    private TilePane grid;
+    private GridPane grid;
     private Stage st2;
     private Scene sc2;
     private DatePicker dp;
-    private TextArea txtAr;
-    private TextField txtFld;
+    private TextField nameFld;
     private Label nameLb;
     private Label dateLb;
     private Label commentLb;
+    private TextArea commentField;
     private Label title;
+    private Stage parentStage;
 
+    private Button confirmBt;
+    private Button cancelBt;
+    private final Messenger msg;
+    private PictureFacade facade;
+    private AnchorPane buttonPane;
 
-    public AttributeEditor(Stage parent){
-        this.bp = new BorderPane();
-        this.grid = new TilePane();
-        this.st2 = new Stage();
-        this.sc2 = new Scene(bp);
-        this.dp = new DatePicker();
-        this.commentLb = new Label("Comment: ");
-        this.nameLb = new Label("Name: ");
-        this.dateLb = new Label("Date: ");
-        this.txtAr = new TextArea();
-        this.txtFld = new TextField();
-        this.title = new Label("header");
+    public AttributeEditor(Stage parentStage, Picture picture,Messenger msg){
+        this.parentStage = parentStage;
+        this.msg = msg;
 
-        grid.getChildren().addAll(nameLb,txtFld,dateLb,dp,commentLb,txtAr);
-        grid.setPrefColumns(2);
-        grid.setPrefRows(3);
-        grid.setPrefTileWidth(200.0);
-        grid.setPrefTileHeight(50.0);
+        facade = Container.getActiveContainer().materialize(PictureFacade.class);
 
-        bp.setPadding(new Insets(5.0,5.0,5.0,5.0));
-        dp.setMaxWidth(200.0);
-        bp.setTop(title);
-        bp.setCenter(grid);
-        st2.setScene(sc2);
-        st2.initOwner(parent);
-        st2.initModality(Modality.APPLICATION_MODAL);
+        initializeNodes(picture);
+        setEventHandlers(picture);
+        setAppearance();
+        putNodesTogether();
     }
 
+
+    private void initializeNodes(Picture picture) {
+        this.bp = new BorderPane();
+        this.title = new Label(msg.translate("editAttributesDialogHeader"));
+        this.grid = new GridPane();
+        this.st2 = new Stage();
+        this.sc2 = new Scene(bp);
+
+        this.nameLb = new Label(msg.translate("editAttributesName"));
+        this.nameFld = new TextField(picture.getName());
+
+        this.dateLb = new Label(msg.translate("editAttributesCreationDate"));
+        this.dp = new DatePicker(picture.getCreationDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+
+        this.commentLb = new Label(msg.translate("editAttributesComment"));
+        this.commentField = new TextArea(picture.getComment());
+
+        this.confirmBt = new Button(msg.translate("confirmBtnImportConfirm"));
+        this.cancelBt = new Button(msg.translate("cancelBtnImportCancel"));
+
+        buttonPane = new AnchorPane();
+    }
+
+    private void setEventHandlers(Picture picture) {
+        cancelBt.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                st2.close();
+            }
+        });
+
+        confirmBt.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if (!nameFld.getText().equals("")) {
+                    picture.setName(nameFld.getText());
+                } else {
+                    //TODO: Exception Handling for this case
+
+                }
+                picture.setCreationDate(Date.from(dp.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()));
+                picture.setComment(commentField.getText());
+                try {
+                    facade.save(picture);
+                    st2.close();
+                } catch (FacadeException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void setAppearance() {
+    	buttonPane.setPadding(new Insets(4,4,4,4));
+
+        commentLb.setAlignment(Pos.TOP_CENTER);
+
+        grid.setGridLinesVisible(false);
+        ColumnConstraints col1 = new ColumnConstraints(80);
+        grid.getColumnConstraints().addAll(col1);
+
+        grid.setHgrow(commentField,Priority.ALWAYS);
+        grid.setVgrow(commentField,Priority.ALWAYS);
+
+        GridPane.setValignment(commentLb, VPos.TOP);
+
+        nameFld.setMaxWidth(Double.MAX_VALUE);
+        commentField.setMaxSize(Double.MAX_VALUE,Double.MAX_VALUE);
+        dp.setMaxWidth(Double.MAX_VALUE);
+
+        AnchorPane.setBottomAnchor(cancelBt,0.0);
+        AnchorPane.setRightAnchor(cancelBt,5.0);
+        AnchorPane.setLeftAnchor(cancelBt,5.0);
+
+        AnchorPane.setBottomAnchor(confirmBt,45.0);
+        AnchorPane.setRightAnchor(confirmBt,5.0);
+        AnchorPane.setLeftAnchor(confirmBt,5.0);
+
+    }
+
+    private void putNodesTogether() {
+        //grid.getChildren().addAll(nameLb,txtFld,dateLb,dp,commentLb,txtAr);
+        bp.setPadding(new Insets(5.0, 5.0, 5.0, 5.0));
+        bp.setTop(title);
+        bp.setCenter(grid);
+        bp.setBottom(buttonPane);
+
+
+        grid.add(nameLb,0,0);
+        grid.add(nameFld,1,0);
+
+        grid.add(dateLb,0,1);
+        grid.add(dp, 1,1);
+
+        grid.add(commentLb,0,2);
+        grid.add(commentField,1,2);
+
+        st2.setScene(sc2);
+        st2.initOwner(parentStage);
+        st2.initModality(Modality.APPLICATION_MODAL);
+        buttonPane.getChildren().addAll(confirmBt,cancelBt);
+    }
 
     public void show(){
         st2.show();
--- Bildverwaltung/bildverwaltung.gui.fx/src/main/java/bildverwaltung/gui/fx/attributeEditor/AttributeEditorTest.java
+++ Bildverwaltung/bildverwaltung.gui.fx/src/main/java/bildverwaltung/gui/fx/attributeEditor/AttributeEditorTest.java
@@ -1,5 +1,13 @@
 package bildverwaltung.gui.fx.attributeEditor;
 
+import bildverwaltung.container.Container;
+import bildverwaltung.container.Scope;
+import bildverwaltung.dao.entity.Picture;
+import bildverwaltung.dao.exception.FacadeException;
+import bildverwaltung.facade.PictureFacade;
+import bildverwaltung.localisation.Messenger;
+import bildverwaltung.localisation.MessengerImpl;
+import bildverwaltung.localisation.TranslatorImpl;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -7,6 +15,8 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class AttributeEditorTest extends Application {
+
+	PictureFacade facade = Container.getActiveContainer().materialize(PictureFacade.class, Scope.APPLICATION);
 
     public void start(Stage primaryStage){
         Button bt = new Button("test");
@@ -16,7 +26,18 @@ public class AttributeEditorTest extends Application {
                 primaryStage.setScene(sc);
 
         bt.setOnAction(e->{
-            AttributeEditor st2 = new AttributeEditor(primaryStage);
+            Picture picture;
+            try {
+                picture = facade.getAllPictures().get(0);
+            } catch (FacadeException e1) {
+                e1.printStackTrace();
+                picture = null;
+            }
+
+            Container.getActiveContainer().addFactory(MessengerImpl.FACTORY);
+            Container.getActiveContainer().addFactory(TranslatorImpl.FACTORY);
+
+            AttributeEditor st2 = new AttributeEditor(primaryStage, picture, Container.getActiveContainer().materialize(Messenger.class));
             st2.show();
         });
 
