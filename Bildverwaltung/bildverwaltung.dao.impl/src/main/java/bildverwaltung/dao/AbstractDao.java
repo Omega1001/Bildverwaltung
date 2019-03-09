Omega1001/Bildverwaltung

commit 8d020b504a766a4aa4e3b1cbba7838461e98592f

Fixed problem where a restart was necessary after adding a picture to an Album

Bildverwaltung/bildverwaltung.dao.impl/src/main/java/bildverwaltung/dao/AbstractDao.java
@@ -87,6 +87,7 @@ public abstract class AbstractDao<E extends UUIDBase> implements CRUDDao<E>,Auto
 		try {
 			E res = em.find(entityClass, key);
 			LOG.trace("Exit get res={}", res);
+			em.refresh(res);
 			return res;
 		} catch (Exception e) {
 			LOG.error("Entity : {} :Error during fetching Entity with key {} : ", entityClass.getSimpleName(), key, e);

Bildverwaltung/bildverwaltung.domainmodell/src/main/java/bildverwaltung/dao/entity/Album.java
@@ -57,7 +57,7 @@ public class Album extends UUIDBase {
 	/**
 	 * @return the pictures
 	 */
-	@ManyToMany(cascade = {CascadeType.PERSIST,CascadeType.MERGE,CascadeType.DETACH})
+	@ManyToMany(cascade = {CascadeType.ALL})
 	@JoinTable
 	public List<Picture> getPictures() {
 		return pictures;

Bildverwaltung/bildverwaltung.domainmodell/src/main/resources/META-INF/persistence.xml
@@ -30,6 +30,8 @@
 			<property name="eclipselink.logging.session" value="false" />
 			<property name="eclipselink.logging.thread" value="false" />
 			<property name="eclipselink.logging.exceptions" value="true" />
+			
+			<property name="eclipselink.query-results-cache" value="false"/>
 		</properties>
 	</persistence-unit>
 </persistence>

Bildverwaltung/bildverwaltung.gui.fx/src/main/java/bildverwaltung/gui/fx/masterview/ToolbarArea.java
@@ -168,12 +168,10 @@ public class ToolbarArea extends RebuildebleSubComponent {
                     Picture pic = viewArea.get().getSelectedPicture().get();
                     Album alb= AlbumSelectionDialog.selectAlbum(msg(),"msgMasterViewAlbumSelecionDlgSelectAlbumToAdd",albumFacade.getAllAlbums(),masterStage.get());
                     if(alb!=null){
-                    	List<Picture> li = alb.getPictures();
-                        if(!li.contains(pic)){
-                            li.add(pic);
-                            alb.setPictures(li);
-                            albumFacade.save(alb);
-                        }
+                    	if(!alb.getPictures().contains(pic)) {
+                    		alb.getPictures().add(pic);
+                    	}
+                    	albumFacade.save(alb);
                     }
 
                 } catch (FacadeException e) {

commit a379933225675d6e0838f43aa9be32c43f1c7471

Fixed Test failures
Fixed Test failures caused by
commit 8d020b504a766a4aa4e3b1cbba7838461e98592f
Fixed problem where a restart was necessary after adding a picture to an
Album

Bildverwaltung/bildverwaltung.dao.impl/src/main/java/bildverwaltung/dao/AbstractDao.java
@@ -22,7 +22,7 @@ import bildverwaltung.dao.exception.ExceptionType;
  * @author jannik
  *
  */
-public abstract class AbstractDao<E extends UUIDBase> implements CRUDDao<E>,AutoCloseable{
+public abstract class AbstractDao<E extends UUIDBase> implements CRUDDao<E>, AutoCloseable {
 
 	private static final Logger LOG = LoggerFactory.getLogger(AbstractDao.class);
 	private EntityManager em;
@@ -86,8 +86,10 @@ public abstract class AbstractDao<E extends UUIDBase> implements CRUDDao<E>,Auto
 		}
 		try {
 			E res = em.find(entityClass, key);
+			if (res != null && em.contains(res)) {
+				em.refresh(res);
+			}
 			LOG.trace("Exit get res={}", res);
-			em.refresh(res);
 			return res;
 		} catch (Exception e) {
 			LOG.error("Entity : {} :Error during fetching Entity with key {} : ", entityClass.getSimpleName(), key, e);
@@ -131,9 +133,10 @@ public abstract class AbstractDao<E extends UUIDBase> implements CRUDDao<E>,Auto
 		}
 		try {
 			E obj = get(key);
-			if(obj == null) {
-				LOG.debug("Entity : {} : Key {} delete is not persistent, doing nothing",entityClass.getSimpleName(),key);
-			}else {
+			if (obj == null) {
+				LOG.debug("Entity : {} : Key {} delete is not persistent, doing nothing", entityClass.getSimpleName(),
+						key);
+			} else {
 				em.remove(obj);
 			}
 		} catch (Exception e) {
@@ -142,7 +145,7 @@ public abstract class AbstractDao<E extends UUIDBase> implements CRUDDao<E>,Auto
 		}
 		LOG.trace("Exit delete");
 	}
-	
+
 	@Override
 	public void close() throws Exception {
 		LOG.trace("Enter close");