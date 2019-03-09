Omega1001/Bildverwaltung

commit c1884722d2504a554d9272117c5530955eeae409

Added all Pictures as album

Bildverwaltung/bildverwaltung.gui.fx/src/main/java/bildverwaltung/gui/fx/masterview/AlbumArea.java
@@ -38,6 +38,7 @@ public class AlbumArea extends RebuildebleSubComponent {
 		TitledPane title = new TitledPane();
 		title.setText(msg().translate("headerTextMasterViewInfoAreaAlbumArea"));
 		albumList = new ListView<>(albums);
+		albums.add(new DBDataRefference<String>(msg().translate("labelMasterViewInfoAreaAlbumAreaAllPictures"), null));
 		albumList.getSelectionModel().selectedItemProperty()
 				.addListener(new ChangeListener<DBDataRefference<String>>() {
 
@@ -48,7 +49,7 @@ public class AlbumArea extends RebuildebleSubComponent {
 							ignoreSelectionUpdate = false;
 							return;
 						}
-						if (newValue == null) {
+						if (newValue == null || newValue.getRefferencedComponentId() == null) {
 							if (!viewArea.get().loadAllPictures()) {
 								albumList.getSelectionModel().select(oldValue);
 							}

Bildverwaltung/bildverwaltung.db-data/src/main/resources/bildverwaltung/dbdata/localisation/resourceStrings.xml
@@ -31,6 +31,7 @@
 		<!-- Info Area -->
 		<!-- Album Area -->
 		<ResourceString id="headerTextMasterViewInfoAreaAlbumArea">Albums</ResourceString>
+		<ResourceString id="labelMasterViewInfoAreaAlbumAreaAllPictures">All Pictures</ResourceString>
 		<!-- Search Area -->
 		<ResourceString id="headerTextMasterViewInfoAreaSearchAreaHeader">Search</ResourceString>
 		<ResourceString id="headerTextMasterViewInfoAreaSearchAreaPicture">Picture</ResourceString>

commit 0f54d65c2b7941d2d2093afe16604fc68f433c5a

Fixed two nullPointerException caused by
commit c1884722d2504a554d9272117c5530955eeae409
Added all Pictures as album

Bildverwaltung/bildverwaltung.gui.fx/src/main/java/bildverwaltung/gui/fx/masterview/AlbumArea.java
@@ -76,7 +76,8 @@ public class AlbumArea extends RebuildebleSubComponent {
 	public void selectAlbum(Album album) {
 		if (album != null) {
 			for (DBDataRefference<String> dbref : albums) {
-				if (dbref.getRefferencedComponentId().equals(album.getId())) {
+				if (dbref.getRefferencedComponentId() != null
+						&& dbref.getRefferencedComponentId().equals(album.getId())) {
 					albumList.getSelectionModel().select(dbref);
 					return;
 				}
@@ -88,19 +89,20 @@ public class AlbumArea extends RebuildebleSubComponent {
 
 	public void removeAlbumWithId(UUID id) {
 		if (id != null) {
-			
+
 			DBDataRefference<String> item = albumList.getSelectionModel().getSelectedItem();
 			if (item != null && id.equals(item.getRefferencedComponentId())) {
 				albumList.getSelectionModel().clearSelection();
 			}
-			
+
 			Iterator<DBDataRefference<String>> it = albums.iterator();
 			while (it.hasNext()) {
-				if (it.next().getRefferencedComponentId().equals(id)) {
+				DBDataRefference<String> next = it.next();
+				if (next.getRefferencedComponentId() != null && next.getRefferencedComponentId().equals(id)) {
 					it.remove();
 				}
 			}
-			
+
 		}
 	}
 