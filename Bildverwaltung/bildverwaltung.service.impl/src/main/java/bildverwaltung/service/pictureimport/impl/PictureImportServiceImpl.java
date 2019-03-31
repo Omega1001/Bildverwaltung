45303db765d0a1021a9413f5992b0c626833b905
matz9 <matthias.sennikow@gmail.com>
2019-03-31 23:11:29 +0200

    removed import statement

--- Bildverwaltung/bildverwaltung.service.impl/src/main/java/bildverwaltung/service/pictureimport/impl/PictureImportServiceImpl.java
+++ Bildverwaltung/bildverwaltung.service.impl/src/main/java/bildverwaltung/service/pictureimport/impl/PictureImportServiceImpl.java
@@ -15,7 +15,6 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.Date;
